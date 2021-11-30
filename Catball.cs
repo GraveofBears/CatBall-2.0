2667a3c33f4f5921e75592f54dfed56ce9eb8e9c
GraveofBears <83285287+GraveofBears@users.noreply.github.com>
2021-11-30 11:03:52 -0500

    Add project files.

---
+++ Catball.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using ItemManager;
+
+namespace CatBall
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class CatBall : BaseUnityPlugin
+    {
+        private const string ModName = "CatBall";
+        private const string ModVersion = "2.0.6";
+        private const string ModGUID = "org.bepinex.plugins.catball";
+
+        public void Awake()
+        {
+            Item CatBall = new Item("ball", "CatBall", "Balls");
+            CatBall.Crafting.Add(CraftingTable.Forge, 3);
+            CatBall.RequiredItems.Add("Iron", 20);
+            CatBall.RequiredItems.Add("WolfFang", 4);
+            CatBall.RequiredItems.Add("Wood", 6);
+            CatBall.RequiredUpgradeItems.Add("Iron", 5);
+            CatBall.RequiredUpgradeItems.Add("WolfFang", 5);
+            CatBall.CraftAmount = 1;
+
+        }
+    }
+}
